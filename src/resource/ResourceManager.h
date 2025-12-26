77c9bd399fb1027897da96fb583794cb3aa76562
superroket169 <isagullu2010@gmail.com>
2025-12-26 23:32:50 +0300

    tested ResourceManager and added doxygen command

--- src/resource/ResourceManager.h
+++ src/resource/ResourceManager.h
@@ -1,8 +1,15 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
-#include "core/Config.h"
+#include "../core/Config.h"
 
+/**
+ * @todo This class is very early state. todos:
+ * - Add user map & car selection system
+ * - Add user map & car selection ui
+ * - Add if no asset create basic shape instead of assets
+ * @brief Resource class that is resources assets
+ */
 namespace Resource
 {
     inline sf::Font Font;
