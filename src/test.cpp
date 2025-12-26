78776371c5f76459b5bb2f71f2475786214fcd48
superroket169 <isagullu2010@gmail.com>
2025-12-26 23:33:18 +0300

    test.cpp for test resource manager

--- src/test.cpp
+++ src/test.cpp
@@ -1,31 +1,29 @@
-#include "input/Input.h"
-#include "core/Time.h"
+#include "resource/ResourceManager.h"
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(800, 800), "test");
-    Time timer;
-    timer.start();
+
+    Resource::loadBackGrounds();
+    Resource::loadCarAssets();
+    Resource::loadFonts();
+    
+    sf::Text text("test", Resource::Font, 24);
+    text.setPosition(200, 50);
+    text.setFillColor(sf::Color::White);
 
     while(window.isOpen())
     {
-        KeyInput::update();
-
-        if(KeyInput::isPressed(Action::Brake))
-        {
-            timer.start();
-        }
-
-        std::cout << timer.elapsedTime() << "\n";
         sf::Event event;
         while(window.pollEvent(event))
         {
             if(event.type == sf::Event::Closed) window.close();
         }
 
-        window.clear();
+        window.clear({0, 0, 0});
+        window.draw(text);
         window.display();
     }
     
